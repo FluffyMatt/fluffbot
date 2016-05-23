9e5b93a3eb28f236f0320e919e975ccede800937
Matt Stephens <matthewstephens@digidev.co.uk>
2016-05-23 16:46:20 +0100

    Example bot commands

--- ping.js
+++ ping.js
@@ -1,25 +1,25 @@
 var Discord = require('discord.io');
+var http = require('http');
 var bot = new Discord.Client({
     autorun: true,
     token: "MTc5MTY3NTM0NjUyNzE5MTA0.Ch8bgw.7X1cBjzvpsb28hwvnfFkyCCvi4c"
 });
 
 bot.on('ready', function() {
-    //console.log(bot.username + " - (" + bot.id + ")");
+    bot.setPresence({
+        game: "FluffyMatt is building"
+    })
 });
 
 bot.on('message', function(user, userID, channelID, message, rawEvent) {
-    if (message === "ping") {
-        bot.sendMessage({
-            to: channelID,
-            message: "http://i.giphy.com/bVDziKRE7jBq8.gif"
+    if (message == '!live') {
+        var live = http.get('http://api.twitch.tv/kraken/streams/'+user, function(response) {
+            var body;
+            response.on('data', function(chunk){
+                body += chunk;
+            });
+            console.log(body);
         });
+
     }
-	if (message === "!sing") {
-		bot.joinVoiceChannel("179950016327188481", function() {
-			bot.getAudioContext({ channel: "179950016327188481", stereo: true}, function(stream) {
-			    stream.playAudioFile('http://www.tonycuffe.com/mp3/tail%20toddle.mp3'); //To start playing an audio file, will stop when it's done.
-			});
-		});
-	}
 });
