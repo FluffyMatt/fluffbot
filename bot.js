5e38dc08b729eb0cb24c6c4ea0f685ca726ce551
Matt Stephens <matthewstephens@digidev.co.uk>
2016-05-24 11:05:04 +0100

    Rewrite and Alpha v0.1

--- bot.js
+++ bot.js
@@ -1,29 +1,49 @@
-var Discord = require('discord.io');
-var http = require('http');
-var bot = new Discord.Client({
-    autorun: true,
-    token: "MTc5MTY3NTM0NjUyNzE5MTA0.Ch8bgw.7X1cBjzvpsb28hwvnfFkyCCvi4c"
+var Discord = require("discord.js");
+var request = require("request");
+var fluffbot = new Discord.Client();
+
+fluffbot.loginWithToken("MTc5MTY3NTM0NjUyNzE5MTA0.Ch8bgw.7X1cBjzvpsb28hwvnfFkyCCvi4c");
+
+fluffbot.on("ready", function(event) {
+    fluffbot.setPlayingGame("Alpha v0.1");
 });
 
-bot.on('ready', function() {
-    bot.setPresence({
-        game: "FluffyMatt is building"
-    })
+fluffbot.on("message", function(message) {
+    if (message.content.indexOf("!gifr") > -1) {
+        var search = searchBuild(message.content.split(" "));
+        request("http://api.giphy.com/v1/gifs/search?q="+search+"&limit=100&api_key=dc6zaTOxFJmzC", function (error, response, body){
+            if (error || response.statusCode !== 200) {
+                fluffbot.replyTTS(message, "Couldn't get an image");
+            }
+            else {
+                response = JSON.parse(body);
+                if (response.pagination.total_count > 100) {
+                    var count = 100;
+                } else {
+                    var count = response.pagination.total_count;
+                }
+                if (count > 0) {
+                    var random = Math.floor((Math.random() * count) + 1);
+                    var gif = response.data[random].url;
+                    fluffbot.replyTTS(message, gif);
+                } else {
+                    fluffbot.replyTTS(message, "No image found");
+                }
+            }
+        });
+    }
 });
 
-bot.on('message', function(user, userID, channelID, message, rawEvent) {
-    if (message == '@FluffBot#7794 Hello') {
-        bot.sendMessage({
-            to: channelID,
-            message: "@"+userID+"I'm still be built sorry!",
-            typing: true,
-        });
+function searchBuild(search) {
+
+    var formattedTerms;
+    var terms = new Array();
+
+    for (var i = 1; i < search.length; i++) {
+        terms.push(search[i]);
     }
-    if (message == '!Hello') {
-        bot.sendMessage({
-            to: userID,
-            message: "I'm still be built sorry!",
-            typing: true,
-        });
-    }
-});
+
+    formattedTerms = terms.join('+');
+
+    return formattedTerms;
+}
