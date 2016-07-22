5f242ef9560b9fee5e3d8f94522c1b0e7d24f1cf
Matt Stephens <matthewstephens@digidev.co.uk>
2016-07-22 13:15:39 +0100

    Update playing title

--- bin/bot.js
+++ bin/bot.js
@@ -11,7 +11,7 @@ discord.loginWithToken(fluffbot.settings.bot_token)
 
 // Initiate the playing game to say the current version
 discord.on('ready', function(event) {
-	this.setPlayingGame('Alpha v2.0')
+	this.setPlayingGame('Alpha v2.0.1 by FluffyMatt')
 })
 
 // Event listener when bot is added to a server
